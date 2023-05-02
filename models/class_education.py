ea05bc0777c8a9391cd3113bab768f086dfffc23
Phattai <luonghuynhphattai@gmail.com>
2023-05-02 20:20:45 +0700

    commit : update class_education

--- models/class_education.py
+++ models/class_education.py
@@ -5,20 +5,23 @@ class Student_class(models.Model):
     _name = "class.student"
     _description = 'Lớp học'
     name = fields.Char(string='Tên lớp')
-    class_ids = fields.Char(string='Mã lớp hoc', readonly=True, default=0)
-    start_date = fields.Date(string='Ngày bắt đầu', required=True)
-    end_date = fields.Date(string='Ngày kết thúc', required=True)
+    reference = fields.Char(string='Mã lớp hoc', readonly=True, default=0)
     student_ids = fields.Many2many(
         comodel_name='student',
         string='học sinh',
         relation='student_class_rel',
         column1='class_ids',
         column2='student_ids')
-    teacher_ids = fields.Many2one('teacher.education',string='Mã giáo viên')
+    teacher_ids = fields.Many2many(
+        comodel_name='teacher.education',
+        string='Giáo viên',
+        relation='teacher_class_rel',
+        column1='teacher_ids',
+        column2='class_ids')
     active = fields.Boolean(default=True)
     @api.model
     def create(self, vals):
         if vals.get('class_ids', 0) == 0:
-            vals['class_ids'] = self.env['ir.sequence'].next_by_code('class.student') or 0
+            vals['reference'] = self.env['ir.sequence'].next_by_code('class.student') or 0
             result = super(Student_class, self).create(vals)
             return result
